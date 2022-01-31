6e07931339168d4fdc64c6babcc8cfd3c4f43957
clrli <72894096+clrli@users.noreply.github.com>
2022-01-30 17:08:58 -0800

    Delete public_html 2 directory

--- public_html 2/classes/pdoconfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $host = 'localhost';
-    $dbname = 'u242050040_swap';
-    $username = 'u242050040_root';
-    $password = 'Asdfjkl1234';
-?>
--- public_html 2/db_conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "u242050040_root", "Asdfjkl1234", "u242050040_swap");
-if (!$conn) {
-    echo "Failed";
-    exit();
-}
-?>
-
